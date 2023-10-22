f9f5b6bf89acd18d262731fc8b3db8aaf517bf8b
foruser <vinhpham2761.gmail.com>
2023-10-22 22:00:00 +0700

    cart

---
+++ BookStore-FA23/src/main/java/com/bookstore/dal/impl/OrderDAO.java
@@ -0,0 +1,19 @@
+package com.bookstore.dal.impl;
+
+import com.bookstore.dal.GenericDAO;
+import com.bookstore.entity.Order;
+import java.util.List;
+
+public class OrderDAO extends GenericDAO<Order>{
+
+    @Override
+    public List<Order> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int insert(Order t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
